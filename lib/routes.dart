6c8feb85cd3af54a484ef90e3b61552589d2f496
clevmania <ernestcleverest@yahoo.com>
2020-09-20 15:53:38 +0100

    added forgot password to routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:merrand/ui/login/login.dart';
 import 'package:merrand/ui/onboard/onboard.dart';
+import 'package:merrand/ui/password/forgot_password.dart';
 
 final Map<String, WidgetBuilder> routes = {
   Onboard.routeName: (context) => Onboard(),
   Login.routeName: (context) => Login(),
+  ForgotPassword.routeName: (context) => ForgotPassword(),
 };
