b2d7af0b8159be20c54d4dd066014a64e45573f5
clevmania <ernestcleverest@yahoo.com>
2020-09-21 04:03:52 +0100

    updated routes with success screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:merrand/ui/login/login.dart';
+import 'package:merrand/ui/login/success.dart';
 import 'package:merrand/ui/onboard/onboard.dart';
 import 'package:merrand/ui/password/forgot_password.dart';
 
@@ -7,4 +8,5 @@ final Map<String, WidgetBuilder> routes = {
   Onboard.routeName: (context) => Onboard(),
   Login.routeName: (context) => Login(),
   ForgotPassword.routeName: (context) => ForgotPassword(),
+  LoginSuccess.routeName: (context) => LoginSuccess()
 };
