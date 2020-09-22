66f6ee909f60b670d930264be9bd28a8587c6098
clevmania <ernestcleverest@yahoo.com>
2020-09-22 11:20:08 +0100

    added register route

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,10 +3,12 @@ import 'package:merrand/ui/login/login.dart';
 import 'package:merrand/ui/login/success.dart';
 import 'package:merrand/ui/onboard/onboard.dart';
 import 'package:merrand/ui/password/forgot_password.dart';
+import 'package:merrand/ui/register/register.dart';
 
 final Map<String, WidgetBuilder> routes = {
   Onboard.routeName: (context) => Onboard(),
   Login.routeName: (context) => Login(),
   ForgotPassword.routeName: (context) => ForgotPassword(),
-  LoginSuccess.routeName: (context) => LoginSuccess()
+  LoginSuccess.routeName: (context) => LoginSuccess(),
+  Register.routeName: (context) => Register()
 };
